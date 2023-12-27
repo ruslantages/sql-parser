aa8eaa7002637cb1294ad3e46cdc8ab73ce984e9
Ruslan Guzhev <ruslan.guzhev@tages.ru>
2023-12-27 17:13:04 +0500

    added random color in output

--- src/App.vue
+++ src/App.vue
@@ -21,34 +21,16 @@
 
 <script setup lang="ts">
 import {ref} from "vue";
+import uniqolor from 'uniqolor';
 
 const sqlPinkCommands = ["begin", "delete", "from", "where", "insert", "into", "values", "update", "commit", "set", "select"]
 const sqlOrangeCommands = ["in"]
-
-const sqlCommands = [...sqlPinkCommands, ...sqlOrangeCommands];
-const findSqlCommandRegexp = new RegExp("(" + sqlCommands.join("|") + ")(?!\\w)", "ig")
-
-const sqlCommandsEndsWithQuotes = [...sqlCommands.map(el => el + '"'), ...sqlCommands.map(el => el + "'")]
-const sqlCommandsEndsWithOpenBracket = sqlCommands.map(el => el + "(")
-const sqlCommandsEndsWithCarriageTransfer = sqlCommands.map(el => el + "\n")
-const sqlCommandsStartsWithQuotes = [...sqlCommands.map(el => '"' + el), ...sqlCommands.map(el => "'" + el)]
-const sqlCommandsStartsWithClosedBracket = sqlCommands.map(el => ")" + el)
-const sqlCommandsStartsWithCarriageTransfer = sqlCommands.map(el => "\n" + el)
-
-const sqlCommandWhichDoesntEndsWithSpace = [
-  ...sqlCommandsEndsWithQuotes,
-  ...sqlCommandsEndsWithOpenBracket,
-  ...sqlCommandsEndsWithCarriageTransfer,
-  ...sqlCommandsStartsWithQuotes,
-  ...sqlCommandsStartsWithClosedBracket,
-  ...sqlCommandsStartsWithCarriageTransfer,
-]
 
 const sqlCommandsAfterWhichCarryOnKeys = ["from", "where", "update", "set", "select", "into"]
 
 const textArea = ref<HTMLTextAreaElement | null>(null);
 const textareaInputValue = ref<string>("INSERT INTO \"units_shipments\" (\"product_guid\", \"oks_guid\", \"production_division_guid\", \"characteristics_guid\", \"measure_unit_type\", \"measure_unit_guid\", \"loaded_at\", \"deleted\") VALUES\n" +
-  "('f52907c7-2f1f-4686-9614-42d161c0da6d', '5bd8e210-45c7-48fd-9581-e5f57ff9bc93', '362c10ea-4bf2-47bd-bb10-9d50357a9d50', NULL, 'Shipment', 'd9d12e98-c853-4474-aa5a-6ae508e7de30', to_timestamp((0)), '0'),\n" +
+  "('{\"name\": \"Ruslan\",\"age\": 26}', '5bd8e210-45c7-48fd-9581-e5f57ff9bc93', '362c10ea-4bf2-47bd-bb10-9d50357a9d50', NULL, 'Shipment', 'd9d12e98-c853-4474-aa5a-6ae508e7de30', to_timestamp((0)), '0'),\n" +
   "('f52907c7-2f1f-4686-9614-42d161c0da6d', '5bd8e210-45c7-48fd-9581-e5f57ff9bc93', '362c10ea-4bf2-47bd-bb10-9d50357a9d50', NULL, 'Spec', 'fe7da683-c82e-4d15-b645-842b95083932', to_timestamp((0)), '0'),\n" +
   "\n" +
   "('f52907c7-2f1f-4686-9614-42d161c0da6d', '3205ee35-7c8d-49fa-83ef-ebe1ce74b4a5', '5f0865a6-6b9d-42bf-a887-b46a14ceb9b8', NULL, 'Shipment', '3b7666e0-5d61-45e4-b162-28a74ad530d3', to_timestamp((0)), '0'),\n" +
@@ -75,36 +57,11 @@ function putKeyFromCommand(word: string) {
   output.value += `<span style="color: blue">${word}</span> `
 }
 
-/*
-let str = `INSERT INTO "values" ("product_guid", "oks_guid", "production_division_guid", "characteristics_guid", "measure_unit_type", "measure_unit_guid", "loaded_at", "deleted") values
-('f52907c7-2f1f-4686-9614-42d161c0da6d', '5bd8e210-45c7-48fd-9581-e5f57ff9bc93', '362c10ea-4bf2-47bd-bb10-9d50357a9d50', NULL, 'Shipment', 'd9d12e98-c853-4474-aa5a-6ae508e7de30', to_timestamp((0)), '0'),
-('f52907c7-2f1f-4686-9614-42d161c0da6d', '5bd8e210-45c7-48fd-9581-e5f57ff9bc93', '362c10ea-4bf2-47bd-bb10-9d50357a9d50', NULL, 'Spec', 'fe7da683-c82e-4d15-b645-842b95083932', to_timestamp((0)), '0'),
-
-('f52907c7-2f1f-4686-9614-42d161c0da6d', '3205ee35-7c8d-49fa-83ef-ebe1ce74b4a5', '5f0865a6-6b9d-42bf-a887-b46a14ceb9b8', NULL, 'Shipment', '3b7666e0-5d61-45e4-b162-28a74ad530d3', to_timestamp((0)), '0'),
-('f52907c7-2f1f-4686-9614-42d161c0da6d', '3205ee35-7c8d-49fa-83ef-ebe1ce74b4a5', '5f0865a6-6b9d-42bf-a887-b46a14ceb9b8', NULL, 'Spec', '051c09db-c7a7-4b19-a8b9-2c77f295d708', to_timestamp((0)), '0');`;
-
-// Регулярное выражение для поиска данных в скобках после VALUES
-let regex = /VALUES\s*([\s\S]*?)(?=;|(\), \())/g;
-
-// Находим данные в скобках после VALUES с помощью регулярного выражения
-let matches = str.replaceAll(" values ", " VALUES ").replaceAll(" values(", " VALUES (").replaceAll("values (", "VALUES (").replaceAll(")values", ") VALUES").replaceAll(") values", ") VALUES").replaceAll(")values(", ") VALUES (").match(regex);
-console.log(matches.length)
-if (matches) {
-  console.log('Найденные элементы:');
-  matches.forEach(match => {
-    console.log(match.replaceAll("'", '"').replaceAll("VALUES (", "(").replaceAll(/,\n(?!\()/g, ", "));
-  });
-} else {
-  console.log('Данные в скобках после VALUES не найдены');
-}
-
-* */
-
 function onSubmit() {
   output.value = ""
 
   const rows = textareaInputValue.value.split(";");
-  rows.filter(Boolean).forEach(row => {
+  rows.filter(Boolean).forEach((row, commandIdx) => {
     row += ";"
     row = row.replace(/(\s|\n)+/g, " ")
     let insertKeys: Array<string> = [];
@@ -112,9 +69,7 @@ function onSubmit() {
 
     if (row.toLowerCase().includes("insert") && row.toLowerCase().includes("values")) {
       row = row.replace(/\)\s?values\s?\(/i, ") VALUES (").replace(/,(?!\s("|')?)/g, ", ");
-      // eslint-disable-next-line no-console
-      console.log("%c row2", "color: yellow", row)
-      // .replace(/'/g, '"')
+
       const insertKeysSubstr = row.match(/\(.*\)(?=\s?values\s?\()/ig)
       if (insertKeysSubstr && insertKeysSubstr[0]) {
         insertKeys = insertKeysSubstr[0]
@@ -122,8 +77,6 @@ function onSubmit() {
           .split(", ")
           .map(el => el.replace(/^("|')|("|')$/g, ""))
       }
-      // eslint-disable-next-line no-console
-      console.log("%c insertKeys", "color: blue", insertKeys)
 
       const insertValuesSubstr = row.match(/\sVALUES\s.*(?=;)/)
       if (insertValuesSubstr && insertValuesSubstr[0]) {
@@ -140,22 +93,62 @@ function onSubmit() {
             )
           )
       }
-      // eslint-disable-next-line no-console
-      console.log("%c insertValues", "color: green", insertValues);
 
       if ((!insertKeys.length || !insertValues.length) || (insertKeys.length && insertValues.length && !insertValues.every(el => el.length === insertKeys.length))) {
         alert("Не удалось распарсить скрипт");
         return;
       }
-      // eslint-disable-next-line no-console
-      console.log("%c row", "color: blue", row)
-      const selectSubstringBeforeValuesRegex = /insert\sinto\s?\S+\s?\(.*\)(?=\s?values\s?\()/ig
-      const substringBeforeValues = row.match(selectSubstringBeforeValuesRegex);
-      if (substringBeforeValues && substringBeforeValues[0]) {
-        row = row.replace(selectSubstringBeforeValuesRegex, "").trim();
+
+      const randomColors = insertKeys.map((el, idx) => {
+        return uniqolor(new Date().getTime() + el + idx)
+      })
+
+      const regExArrayBeforeKeys = row.match(/insert\sinto\s?("|')?\w*("|')?\s?/i)
+      let substrBeforeKeys = ""
+      if (regExArrayBeforeKeys && regExArrayBeforeKeys[0]) {
+        substrBeforeKeys = regExArrayBeforeKeys[0]
       }
-      // eslint-disable-next-line no-console
-      console.log("%c row", "color: yellow", row)
+
+      output.value += substrBeforeKeys.trim() + "</br></br>("
+
+      insertKeys.forEach((el, idx) => {
+        output.value += `<span
+          data-key-idx="${idx}"
+          data-row-idx="${commandIdx}"
+          style="
+            background: ${randomColors[idx].color};
+            color: ${randomColors[idx].isLight ? "black" : "white"};
+          "
+        >${el}</span>`
+        if (idx !== insertKeys.length - 1) {
+          output.value += ", "
+        }
+      })
+
+      output.value += ")</br></br>VALUES"
+      insertValues.forEach((el, idx) => {
+        output.value += "</br></br>("
+
+        el.forEach((keysValues, keysValuesIdx) => {
+          output.value += `<span
+          data-key-idx="${keysValuesIdx}"
+          data-row-idx="${commandIdx}"
+          style="
+            background: ${randomColors[keysValuesIdx].color};
+            color: ${randomColors[keysValuesIdx].isLight ? "black" : "white"};
+          "
+        >${keysValues}</span>`
+          if (keysValuesIdx !== el.length - 1) {
+            output.value += ", "
+          }
+        })
+
+        if (idx === insertValues.length - 1) {
+          output.value += ");"
+        } else {
+          output.value += "),"
+        }
+      })
     } else {
       const wordsFromRow = row.trim().split(" ").map(el => el.trim());
 
@@ -184,12 +177,12 @@ function onSubmit() {
         }
       })
     }
-    
+    output.value += "</br></br></br>"
   })
 }
 </script>
 
-<style scoped>
+<style>
 main {
   width: 100%;
   max-width: 95%;
@@ -251,5 +244,6 @@ main {
   border-color: green;
   resize: both;
   overflow: auto;
+  line-height: 1.5;
 }
 </style>
